79f6a4003532a949303eb60dc85de665e9416cc2
Richard <richardramos112094@gmail.com>
2024-06-14 23:32:56 +0800

    test1234

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaXT_PUBLseUrl = "https://cvfcuyxuswjwesoqknqn.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2ZmN1eXh1c3dqd2Vzb3FrbnFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgzNTIxNDcsImV4cCI6MjAzMzkyODE0N30.QaT1xa_yXY71OetpCkAPDa6VjcmHIf9K0hgklQMC0p8"
-const supabase = createClient(supabaXT_PUBLseUrl, supabaseKey)
+const supabaseUrl = process.env.NEIC_SUPABASE_URL ?? ''
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ''
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
